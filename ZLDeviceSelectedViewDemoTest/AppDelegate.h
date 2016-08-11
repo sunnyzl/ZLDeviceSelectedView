a79c764deb5b75eb4d35b95eb72af8f7342ae2cb
zhaoliang <86239715@163.com>
2016-08-11 16:42:44 +0800

    Initial Commit

---
+++ ZLDeviceSelectedViewDemoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLDeviceSelectedViewDemoTest
+//
+//  Created by zhaoliang on 16/8/11.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZLDeviceSelectedViewDemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLDeviceSelectedViewDemoTest
+//
+//  Created by zhaoliang on 16/8/11.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
